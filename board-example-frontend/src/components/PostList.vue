24b2a8c080f79fb4aa72dd88615537ca261a2882
hjkim4518 <hjkim4518@cslee.co.kr>
2019-11-05 15:07:01 +0900

    게시물 조회 껍데기 완료

---
+++ board-example-frontend/src/components/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <table>
+            <colgroup>
+                <col style="width: 10%;" />
+                <col style="width: 60%;" />
+                <col style="width: 10%;" />
+                <col style="width: 20%;" />
+            </colgroup>
+            <thead>
+                <tr>
+                    <th scope="col">번호</th>
+                    <th scope="col">제목</th>
+                    <th scope="col">작성자</th>
+                    <th scope="col">작성일</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td scope="col">1</td>
+                    <td scope="col">게시글의 제목이 노출됩니다.[2]</td>
+                    <td scope="col">홍길동</td>
+                    <td scope="col">2018-01-01 09:00</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PostList'
+}
+</script>
---
+++ board-example-frontend/src/pages/PostListPage.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="post-list-page">
+        <h1>포스트 게시글</h1>
+        <post-list />
+    </div>
+</template>
+
+<script>
+import PostList from '@/components/PostList.vue'
+export default {
+    name: 'PostListPage',
+    components: { PostList }
+}
+</script>
