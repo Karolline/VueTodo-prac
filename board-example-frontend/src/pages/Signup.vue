942d92fbb8969eac18df6ed33c0ba1d1de6b1ff5
hjkim4518 <hjkim4518@cslee.co.kr>
2019-11-07 14:08:49 +0900

    회원가입 기능 구현

---
+++ board-example-frontend/src/pages/Signup.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="sign-up-page">
+        <div class="sign-up-page">
+            <h3>회원가입</h3>
+            <signup-form @submit="onSubmit" />
+            <p>이미 가입하셨나요? <router-link :to="{ name: 'Signup' }">로그인하러 가기</router-link></p>
+        </div>
+    </div>
+</template>
+
+<script>
+import SignupForm from '@/components/SignupForm.vue'
+import api from '@/api'
+
+export default {
+    name: 'Signup',
+    components: {
+        SignupForm
+    },
+    methods: {
+        onSubmit (payload) {
+            const { name, email, password } = payload
+            api.post('/auth/signup', { email, password, name })
+            .then(res => {
+                alert('회원가입이 완료되었습니다.')
+                this.$router.push('/')
+            })
+            .catch(err => {
+                alert(err.response.data.msg)
+            })
+        }
+    }
+}
+</script>
