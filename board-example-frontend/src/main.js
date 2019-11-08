fa785034af3a1713e5eadabdbc5b04137729fe8b
hjkim4518 <hjkim4518@cslee.co.kr>
2019-11-08 13:02:38 +0900

    게시물 작성 컴포넌트 추가(로그인한 회원만 접근가능)

--- board-example-frontend/src/main.js
+++ board-example-frontend/src/main.js
@@ -8,16 +8,22 @@ import Cookies from 'js-cookie'
 
 Vue.config.productionTip = false
 
-const savedToken = Cookies.get('accessToken')
-if (savedToken) {
-  store.dispatch('signinByToken', savedToken)
+function init () {
+  const savedToken = Cookies.get('accessToken')
+  if (savedToken) {
+    return store.dispatch('signinByToken', savedToken)
+  }
+  else {
+    return Promise.resolve()
+  }
 }
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  store,
-  components: { App },
-  template: '<App/>'
+init().then(res => {
+  new Vue({
+    el: '#app',
+    router,
+    store,
+    components: { App },
+    template: '<App/>'
+  })
 })
